e63200306e0a0a9d6050c55abfdbb6610455a99c
KarenPHS <hsaun8169@gmail.com>
2021-10-01 00:37:58 +0800

    flink cluster setup

--- Startup.sh
+++ Startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# [NN zookeeper format]
-echo "Zookeeper formating..."
-docker exec -i active-nn /bin/bash -c  "/usr/local/hadoop/bin/hdfs zkfc -formatZK"
+# [Zookeeper start]
+echo "Starting Zookeeper on znode1, znode2 and znode3..."
+docker exec -i znode1 /bin/bash -c  "/usr/local/zookeeper/bin/zkServer.sh start" && docker exec -i znode2 /bin/bash -c  "/usr/local/zookeeper/bin/zkServer.sh start" && docker exec -i znode3 /bin/bash -c  "/usr/local/zookeeper/bin/zkServer.sh start"
 sleep 2
 
 # [Journal start]
@@ -15,19 +15,19 @@ echo "NameNode formating..."
 docker exec -i active-nn /bin/bash -c "/usr/local/hadoop/bin/hdfs namenode -format"
 sleep 2
 
-# [Standby get data]
-echo "Getting from active-nn..."
-docker exec -i standby-nn /bin/bash -c "/usr/local/hadoop/bin/hdfs namenode -bootstrapStandby"
+# [NN zookeeper format]
+echo "Zookeeper formating..."
+docker exec -i active-nn /bin/bash -c  "/usr/local/hadoop/bin/hdfs zkfc -formatZK"
 sleep 2
 
 # [NN start]
 echo "Namenode starting on active-nn..."
-docker exec -i standby-nn /bin/bash -c "/usr/local/hadoop/bin/hdfs --daemon start namenode"
+docker exec -i active-nn /bin/bash -c "/usr/local/hadoop/bin/hdfs --daemon start namenode"
 sleep 2
 
-# [Zookeeper start]
-echo "Starting Zookeeper on znode1, znode2 and znode3..."
-docker exec -i znode1 /bin/bash -c  "/usr/local/zookeeper/bin/zkServer.sh start" && docker exec -i znode2 /bin/bash -c  "/usr/local/zookeeper/bin/zkServer.sh start" && docker exec -i znode3 /bin/bash -c  "/usr/local/zookeeper/bin/zkServer.sh start"
+# [Standby get data]
+echo "Getting from active-nn..."
+docker exec -i standby-nn /bin/bash -c "/usr/local/hadoop/bin/hdfs namenode -bootstrapStandby"
 sleep 2
 
 # [NN stop]
@@ -35,11 +35,15 @@ echo "Stoping NN on active-nn..."
 docker exec -i active-nn /bin/bash -c "/usr/local/hadoop/bin/hdfs --daemon stop namenode"
 sleep 2
 
+# [Journal stop]
+echo "Stoping all Journalnode"
+docker exec -i journalnode1 /bin/bash -c  "/usr/local/hadoop/bin/hdfs --daemon stop journalnode" && docker exec -i journalnode2 /bin/bash -c  "/usr/local/hadoop/bin/hdfs --daemon stop journalnode" && docker exec -i journalnode3 /bin/bash -c  "/usr/local/hadoop/bin/hdfs --daemon stop journalnode"
+sleep 2
+
 # [All NN start]
 echo "Starting NN & workers and then conneting to "
 docker exec -i active-nn /bin/bash -c "/usr/local/hadoop/sbin/start-dfs.sh"
 sleep 5
-
 # [RM && HS start]
 echo "Starting RM..."
 docker exec -i active-rm /bin/bash -c "/usr/local/hadoop/sbin/start-yarn.sh"
